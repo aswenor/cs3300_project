8489ad51ca1b64bb8fd8f2f648a542368db1144f
Abigail Swenor <aswenor@uccs.edu>
2021-11-29 15:54:33 +0000

    Iteration 5: Working on tests (homepage links tested)

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -6,5 +6,34 @@ RSpec.feature "Visiting the homepage", type: :feature do
     visit root_path
     expect(page).to have_text("Projects")
   end
+
+  # Test that the user can view links
+  scenario "The visitor should see edit profile" do
+    visit root_path
+    if user_signed_in
+      expect(page).to have_link("Edit proflie")
+    end
+  end
+
+  scenario "The visitor should see logout" do
+    visit root_path
+    if user_signed_in
+      expect(page).to have_link("Logout")
+    end
+  end
+
+  scenario "The visitor should see signup" do
+    visit root_path
+    if not(user_signed_in)
+      expect(page).to have_link("Sign up") 
+    end
+  end
+
+  scenario "The visitor should see login" do
+    visit root_path
+    if not(user_signed_in)
+      expect(page).to have_link("Login")
+    end
+  end
 end
 
